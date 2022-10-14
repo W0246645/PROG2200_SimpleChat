f97f6fd9f341b02cfb96259f309fe177e8e03f94
Matthew Covey <w0246645@campus.nscc.ca>
2022-10-14 11:47:38 -0300

    Third and Fourth Phases complete (InCL3)

--- SimpleChat/SimpleChat/SimpleChat/Program.cs
+++ SimpleChat/SimpleChat/SimpleChat/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SimpleChat
@@ -25,6 +26,7 @@ namespace SimpleChat
             */
 
             // ------------------------ Second Phase -------------------------
+            /*
             while (true)
             {
                 //1.Run as Client vs Server
@@ -36,6 +38,52 @@ namespace SimpleChat
                 //5.Let the user quit
                 Console.WriteLine($"You typed {userKey.Key}");
                 Console.Beep();
+            }
+            */
+
+            // ------------------------ Third Phase -------------------------
+            /*
+            while (true)
+            {
+                //1.Run as Client vs Server
+                Console.WriteLine("Listening for messages");
+                if (Console.KeyAvailable)
+                {
+                    //2.User input mode: when user press "I" key.            
+                    var userKey = Console.ReadKey();
+                    //3.Display incoming message
+                    //4.Allow for input
+                    //5.Let the user quit
+                    Console.WriteLine($"\nYou typed {userKey.Key}");
+                    Console.Beep();
+                    Thread.Sleep(500);
+                }
+            }
+            */
+
+            // ------------------------ Fourth Phase -------------------------
+            while (true)
+            {
+                //1.Run as Client vs Server
+                Console.WriteLine("Listening for messages");
+                if (Console.KeyAvailable)
+                {
+                    //2.User input mode: when user press "I" key.            
+                    var userKey = Console.ReadKey();
+                    if (userKey.Key == ConsoleKey.I)
+                    {
+                        Console.Write("'I' is PRESSED >>"); //insertion mode
+                        Console.Beep();
+                        Thread.Sleep(500);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nYou typed {userKey.Key}");
+                        Console.Beep();
+                        Thread.Sleep(500);
+                    }
+                }
             }
         }
     }
