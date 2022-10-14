4a3d2fc93fb263eb4b1699b82eef6c2790f28ef1
Matthew Covey <w0246645@campus.nscc.ca>
2022-10-14 12:54:49 -0300

    Sixth Phase complete (InCL4)

--- SimpleChat/SimpleChat/SimpleChat/Program.cs
+++ SimpleChat/SimpleChat/SimpleChat/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -89,6 +90,7 @@ namespace SimpleChat
             */
 
             // ------------------------ Fifth Phase -------------------------
+            /*
             if (args.Contains("-server"))
             {
                 Console.WriteLine("Server");
@@ -106,6 +108,48 @@ namespace SimpleChat
                     //2.User input mode: when user press "I" key.            
                     var userKey = Console.ReadKey();
                     if (userKey.Key == ConsoleKey.I)
+                    {
+                        Console.Write("'I' is PRESSED >>"); //insertion mode
+                        Console.Beep();
+                        Thread.Sleep(500);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nYou typed {userKey.Key}");
+                        Console.Beep();
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+            */
+
+            // ------------------------ Sixth Phase -------------------------
+            if (args.Contains("-server"))
+            {
+                Console.WriteLine("Server");
+                var server = new Server();
+            }
+            else
+            {
+                Console.WriteLine("Client");
+                var client = new Client();
+            }
+
+            while (true)
+            {
+                //1.Run as Client vs Server
+                Console.WriteLine("Listening for messages");
+                if (Console.KeyAvailable)
+                {
+                    //2.User input mode: when user press "I" key.            
+                    var userKey = Console.ReadKey();
+                    if (userKey.Key == ConsoleKey.Escape)
+                    {
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    }
+                    else if (userKey.Key == ConsoleKey.I)
                     {
                         Console.Write("'I' is PRESSED >>"); //insertion mode
                         Console.Beep();
