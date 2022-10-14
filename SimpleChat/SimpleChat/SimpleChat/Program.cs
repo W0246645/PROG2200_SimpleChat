6ebc761c505edbaf0a29f1b34a7aa8ebf81534ba
Matthew Covey <w0246645@campus.nscc.ca>
2022-10-14 11:31:43 -0300

    First Phase complete

---
+++ SimpleChat/SimpleChat/SimpleChat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1
+            //1.Run as Client vs Server
+            Console.WriteLine("Listening for messages");
+            //2.User input mode: when user press "I" key.            
+            var userKey = Console.ReadKey();
+            //3.Display incoming message
+            //4.Allow for input
+            //5.Let the user quit
+            Console.WriteLine($"You typed {userKey.Key}");
+            Console.Beep();
+        }
+    }
+}
